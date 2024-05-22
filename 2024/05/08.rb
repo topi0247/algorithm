1676f28492c1262a6516763bf5fbb9d914cd036e
topi0247 <karubona.ra247@gmail.com>
2024-05-22 22:52:21 +0900

    between a and bの修正

--- 2024/05/08.rb
+++ 2024/05/08.rb
@@ -1,7 +1,18 @@
-a = gets.to_i
-b = gets.to_i
-x = gets.to_i
+# a = gets.to_i
+# b = gets.to_i
+# x = gets.to_i
 
-answer = (a..b).inject(0) {|count,i| count += 1 if i % x == 0; count}
+# answer = (a..b).inject(0) {|count,i| count += 1 if i % x == 0; count}
 
+# puts answer
+
+abx = gets.split
+a = abx[0].to_i
+b = abx[1].to_i
+x = abx[2].to_i
+
+answer = (b/x) - (a/x)
+if a % x == 0
+  answer += 1
+end
 puts answer
